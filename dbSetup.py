9e8982020c8390e25d4373e020e13b4f275565d4
SSmith2002 <sebsmith2002@gmail.com>
2023-09-15 12:17:05 +0100

    finished dbSetup.py

--- dbSetup.py
+++ dbSetup.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import random
 
 mydb = mysql.connector.connect(
     host = "localhost",
@@ -8,6 +9,19 @@ mydb = mysql.connector.connect(
 )
 
 cursor = mydb.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS ducks (id INTEGER, longid INTEGER, found BOOLEAN)")
+cursor.execute("DROP TABLE IF EXISTS ducks")
+cursor.execute("CREATE TABLE ducks (id INTEGER PRIMARY KEY, longid INTEGER, found BOOLEAN)")
 
+for i in range(64):
+    firstdigit = random.randint(1,9)
+    longid = str(firstdigit)
+    for k in range(7):
+        digit = random.randint(0,9)
+        longid += str(digit)
+    
+    longid = int(longid)
+    print(longid)
 
+    cursor.execute("INSERT INTO ducks VALUES (%d,%d,FALSE)" % (i,longid))
+
+mydb.commit()
